9f0c90bedcc6a25d6b556aa07a28abbe9f5508e5
enlike20 <118214270+enlike20@users.noreply.github.com>
2022-11-22 20:27:03 +0100

    Update EjercicioGIT.java

--- ProyectoGitRamasEjemplo/src/EjercicioGIT.java
+++ ProyectoGitRamasEjemplo/src/EjercicioGIT.java
@@ -3,7 +3,7 @@ public class EjercicioGIT {
 
 	public static void main(String[] args) {
 
-System.out.println("Hola mundo");
+System.out.println("Wola");
 	}
 public static void imprimeSaludo() { 
 System.out.println(“Hei”);	
